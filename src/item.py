7ef6b0e4d0e0aa7faef7543662a326f2bc219abd
vanila <idegtiareov@mail.ru>
2023-07-15 05:35:39 +0300

    1 Homework

--- src/item.py
+++ src/item.py
@@ -6,14 +6,11 @@ class Item:
     all = []
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
-        """
-        Создание экземпляра класса item.
+        self.name = name
+        self.price = price
+        self.quantity = quantity
 
-        :param name: Название товара.
-        :param price: Цена за единицу товара.
-        :param quantity: Количество товара в магазине.
-        """
-        pass
+
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +18,11 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        total_price = self.price * self.quantity
+        return total_price
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price *= self.pay_rate
